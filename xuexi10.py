ba6b2d1b9c303b320b2072ba196b47e99eb51bdc
aidc-shen <148891619+aidc-shen@users.noreply.github.com>
2023-10-26 16:29:18 +0800

    Add files via upload

---
+++ xuexi10.py
@@ -0,0 +1,13 @@
+temperature_list={"111":36.4,"112":36.6,"113":38.6}
+for staff_id,temperature in temperature_list. items():
+    if temperature>=38:
+        print(staff_id,temperature)
+
+
+total=0
+for num in range(1,101,2):
+    total=total+num
+print(total)
+
+      
+
